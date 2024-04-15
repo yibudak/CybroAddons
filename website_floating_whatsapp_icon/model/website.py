407232ce85eafce0655854ec4acbf963f57738d3
yibudak <yibudak@gmail.com>
2024-04-15 10:02:11 +0300

    [IMP] Whatsapp number translatable

--- website_floating_whatsapp_icon/model/website.py
+++ website_floating_whatsapp_icon/model/website.py
@@ -26,4 +26,4 @@ from odoo import fields, models
 class Website(models.Model):
     _inherit = 'website'
 
-    mobile_number = fields.Char(string='Mobile Number')
+    mobile_number = fields.Char(string='Mobile Number', translate=True)
